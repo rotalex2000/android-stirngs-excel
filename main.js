8b7a5eb556f908727231e6b7e52b7eb188d67cc9
Alexandru Rotariu <alexandru.rotariu@emanuel.ro>
2021-02-16 21:43:38 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+$("[data-toggle=popover]").popover();
+
+function exportStringsToClipoard() {
+    var exportedStrings = convertRawToTabular(document.getElementById("rawInput").value);
+    const tempT = document.createElement('textarea');
+    tempT.value = exportedStrings;
+    document.body.appendChild(tempT);
+    tempT.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempT);
+}
+
+function convertRawToTabular(rawStrings) {
+    var nextCell = '	';
+    return removeUnTranslatableStrings(rawStrings)
+        .replace('\n', '') // first new line
+        .replaceAll(/^\s*[\r\n]/gm, '') // blank lines
+        .replaceAll(/  +/g, '') // redundant spaces
+        .replaceAll('<resources>', '')
+        .replaceAll('<string name="', nextCell)
+        .replaceAll('">', nextCell)
+        .replaceAll('</string>', '')
+        .replaceAll('</resources>', '')
+        .replaceAll(/<!--(\s)?/g, '\n')
+        .replaceAll(/(\s)?-->(\n)/g, '');
+}
+
+function removeUnTranslatableStrings(strings) {
+    var lines = strings.split('\n');
+    var i = 0;
+    while (i < lines.length) {
+        if (lines[i].includes('translatable="false"')) {
+            lines.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+    return lines.join('\n');
+}
